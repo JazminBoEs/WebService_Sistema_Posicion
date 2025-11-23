1b1a4a4bf834796abbffba174f7ab5b703c7ae6d
JazminBoEs <A01564215@tec.mx>
2025-11-23 23:21:17 +0000

    cambio de ruta

--- src/main/java/com/sistema/raspberry/controller/AprilTagController.java
+++ src/main/java/com/sistema/raspberry/controller/AprilTagController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/apriltags")
+@RequestMapping("/apriltags")
 @CrossOrigin(origins = "*")
 public class AprilTagController {
 
