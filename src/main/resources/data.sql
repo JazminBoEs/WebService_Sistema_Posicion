d3feb4ea8dbde041508bab6a295ccc03ddf51e42
JazminBoEs <A01564215@tec.mx>
2025-11-23 22:54:04 +0000

    cambio a bd h2 para pruebas

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- Insertar AprilTags precargados
+INSERT INTO April_Tag (id_April_Tag, x, y, orientacion) VALUES
+(0, 0.0, 1.11, 0),
+(1, 1.230, 0.0, 90),
+(2, 0.0, 2.197, 0),
+(3, 3.60, 1.038, 180),
+(4, 2.634, 0.0, 90),
+(5, 2.51, 2.8, 270),
+(6, 1.35, 2.8, 270),
+(7, 3.60, 1.985, 180);
